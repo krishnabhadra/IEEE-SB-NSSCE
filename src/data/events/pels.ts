ace439c232d68a243b785b2294e8d77f3b2fde1f
bhadrakrishna <prarthanaprarthana2008@gmail.com>
2026-06-17 01:02:59 +0530

    added pels new event

--- src/data/events/pels.ts
+++ src/data/events/pels.ts
@@ -49,5 +49,22 @@ export const pelsEvents: Event[] = [
       "https://sb-dataset.vercel.app/events/pels/e3/1.jpg"
     ]
   },
+  {
+  id: "pels-e4",
+  slug: "poster-design-competition",
+  title: "POSTER DESIGN COMPETITION",
+  description: "Unleash your creativity to imagine the future of energy! As part of UNPLUGGED 2026 and the IEEE PELS Day Celebration, IEEE PELS SBC NSSCE presents a Poster Design Competition on the theme 'Powering the Future'. Showcase how power electronics is transforming the world through renewable energy, electric vehicles, smart grids, and other innovations driving a sustainable tomorrow.",
+  date: "2026-06-18T00:00:00Z",
+  venue: "Online",
+  price: "Free",
+  societyId: "pels",
+  status: "upcoming",
+  banner: "https://sb-dataset.vercel.app/events/pels/e4/1.jpg",
+  tags: ["Poster Design", "Competition", "IEEE PELS Day", "Power Electronics"],
+  gallery: [
+    "https://sb-dataset.vercel.app/events/pels/e4/1.jpg"
+  ],
+},
+
 
 ];
